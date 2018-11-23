9e055ad9ff7d27715501bce70627a9048b15338e
Iso Takuma <tak@Iso-no-MacBook-Air.local>
2018-11-23 12:08:41 +0530

    aa

--- flip.py
+++ flip.py
@@ -1,18 +1,7 @@
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as uReq
-import sys
 
-
-my_url = 'https://www.99acres.com/rent-paying-guest-pg-in-koramangala-bangalore-south-ffid'
-
-headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"
-        }
-
-request = urllib.request.Request(url, headers=headers)
-response = urllib.request.urlopen(request)
-print (response.getcode())
-print (response.read())
+my_url = 'https://www.flipkart.com/search?q=iphone&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=off&as=off'
 
 uClient=uReq(my_url)
 
@@ -21,59 +10,59 @@ page_html=uClient.read()
 
 uClient.close()
 page_soup =soup(page_html, "html.parser")
-containers = page_soup.findAll("div", {"class": "wrapttl"})
+containers = page_soup.findAll("div", {"class": "_3O0U0u"})
 container = containers[0]
 
-print(len(containers))
+# print(len(containers))
 
 # print(soup.prettify(containers[23])) 
-# price = container.findAll("div", {"class": "col col-5-12 _2o7WAb"})
-# print(price[0].text)
+price = container.findAll("div", {"class": "col col-5-12 _2o7WAb"})
+print(price[0].text)
 
-# #for name
-# print(container.div.img["alt"])
-# ratings = container.findAll("div", {"class": "niH0FQ"})
+#for name
+print(container.div.img["alt"])
+ratings = container.findAll("div", {"class": "niH0FQ"})
 
-# # # container=containers[0]
-# print(ratings[0].text)
-
-
-# # creating file
-# filename="products.csv"
-# f=open(filename,"w")
-
-# headers="Product_Name, Pricing, Rating\n"
-# f.write(headers)
-
-# for container in containers:
-#     product_name=container.div.img["alt"]
-
-#     price_container = container.findAll(
-#         "div", {"class": "col col-5-12 _2o7WAb"})
-#     price=price_container[0].text.strip()
-
-#     rating_container = container.findAll("div", {"class": "niH0FQ"})
-#     rating=rating_container[0].text
+# # container=containers[0]
+print(ratings[0].text)
 
 
-#     # print("product_name:" + product_name)
-#     # print("price:"+ price)
+# creating file
+filename="products.csv"
+f=open(filename,"w")
 
-#     # print("ratings:" + rating)
+headers="Product_Name, Pricing, Rating\n"
+f.write(headers)
 
-#     #string
+for container in containers:
+    product_name=container.div.img["alt"]
 
-#     trim_price=''.join(price.split(','))
-#     rm_rupee=trim_price.split("₹")
-#     add_rs_price ="Rs." + rm_rupee[1]
+    price_container = container.findAll(
+        "div", {"class": "col col-5-12 _2o7WAb"})
+    price=price_container[0].text.strip()
+
+    rating_container = container.findAll("div", {"class": "niH0FQ"})
+    rating=rating_container[0].text
+
+
+    # print("product_name:" + product_name)
+    # print("price:"+ price)
+
+    # print("ratings:" + rating)
+
+    #string
+
+    trim_price=''.join(price.split(','))
+    rm_rupee=trim_price.split("₹")
+    add_rs_price ="Rs." + rm_rupee[1]
     
-#     split_price=add_rs_price.split("E")
-#     final_price=split_price[0]
+    split_price=add_rs_price.split("E")
+    final_price=split_price[0]
 
-#     split_rating = rating.split(" ")
-#     final_rating=split_rating[0]
+    split_rating = rating.split(" ")
+    final_rating=split_rating[0]
 
-#     print(product_name.replace("," ,"|")+ "," + final_price + "," + final_rating + "\n")
-#     f.write(product_name.replace(",", "|")+ "," +final_price + "," +final_rating + "\n")
+    print(product_name.replace("," ,"|")+ "," + final_price + "," + final_rating + "\n")
+    f.write(product_name.replace(",", "|")+ "," +final_price + "," +final_rating + "\n")
 
-# f.close()
+f.close()
